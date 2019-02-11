6f2dc8fce91303ecd62cea671e4702bba0bd1506
Gaurav Sharma <gaurav.sharma@zenways.io>
2019-02-11 14:25:18 +0530

    npx create-react-app and webpack issue

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import ReactDOM from 'react-dom';
 // My Code
 // import { MoviesTable } from "./components/MoviesTable";
 
+/**
+ * Webpack Issue with newly created react app
+ * https://github.com/webpack/webpack/issues/8768
+ * https://stackoverflow.com/questions/54612657/why-does-npm-start-give-error-in-node-js
+ */
+
+
+
 import { PapaParse } from "./components/PapaParse";
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
